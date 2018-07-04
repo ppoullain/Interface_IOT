103c04fb1756ba292d6695f40bce511e8dc44819
ppoullain <pierre.poullain22@gmail.com>
2018-07-04 21:00:52 +0200

    migration angular6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { ListDevicesComponent } from './list-devices/list-devices.component';
+import { BezierComponent } from './bezier/bezier.component';
+import { InfoCommandesComponent } from './info-commandes/info-commandes.component';
+import { SvgCanvasComponent } from './svg-canvas/svg-canvas.component';
+import { DeviceComponent } from './device/device.component';
+import { DeviceService } from './device/device.service';
+import { NgbdModalBasic } from './device/modal-device';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListDevicesComponent,
+    BezierComponent,
+    InfoCommandesComponent,
+    SvgCanvasComponent,
+    DeviceComponent,
+    NgbdModalBasic
+  ],
+  entryComponents: [ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [ DeviceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
